a29c5215937014fcc0a31be491ccc4a4485ce3e4
harshita <harshita1225@gmail.com>
2022-09-23 11:57:44 +0200

    edited

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ btnClose.addEventListener("click", function () {
   popup.style.display = "none";
 });
 
-document.addEventListener("mouseenter", function (e) {
+/* document.addEventListener("mouseenter", function (e) {
   const y = e.offsetY;
   if (y > 0 && y < 10) {
+    popup.style.display = "flex";
+  } else {
+    popup.style.display = "none";
+  }
+}); */
+
+document.addEventListener("mousemove", function (e) {
+  const y = e.clientY;
+  if (y > 10) {
     popup.style.display = "flex";
   } else {
     popup.style.display = "none";
